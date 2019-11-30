9434f829449b7d62e95ea4c2a2e73861651db119
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2019-11-30 10:56:11 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './app-state';
+import { Product } from './product';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'redux-store';
+  title = 'app';
+  products$ = this.store.select('products');
+  newProduct: string;
+  id = 1;
+  selectedProduct;
+
+  constructor(private store: Store<AppState>) {}
+
+  select(product) {
+    this.selectedProduct = { ...product };
+  }
+
+  save() {
+    this.store.dispatch({
+      type: 'ADD',
+      payload: {
+        name: this.newProduct,
+        id: this.id++
+      }
+    });
+  }
+
+  delete(product: Product) {
+    this.store.dispatch({
+      type: 'REMOVE',
+      payload: product
+    });
+  }
+
+  update() {
+    this.store.dispatch({
+      type: 'UPDATE',
+      payload: this.selectedProduct
+    });
+    this.selectedProduct = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const productsReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD':
+      return [...state, { ...action.payload }];
+    case 'REMOVE':
+      return state.filter(p => p.id !== action.payload.id);
+    case 'UPDATE':
+      let product = state.find(p => (p.id !== action.payload.id) === p.id);
+      product = { ...product, ...action.payload };
+
+      const products = state.filter(p => action.payload.id !== p.id);
+      return [product, ...products];
+    default:
+      return state;
+  }
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    StoreModule.forRoot({
+      products: productsReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
